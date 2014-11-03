f9067783f9ebcfbb4452a02ae24461f15acd5b17
AAbdelhady <a.said.abdelhady@gmail.com>
2014-11-03 21:02:38 +0200

    Update Dish.java

--- RestaurantGameCodeGen/src/Dish.java
+++ RestaurantGameCodeGen/src/Dish.java
@@ -1,15 +1,4 @@
 public class Dish extends Menu_Item
 {
 	public int calories;
-	
-	public Dish randomlyChooseDish( )
-	{
-		return null;
-	}
-	
-	public Dish addNewDish( String dishName, Quality dishQuality, int dishPrice, int dishCalories )
-	{
-		return null;
-	}
-	
 }
