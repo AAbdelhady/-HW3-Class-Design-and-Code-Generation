d09ba10ed8ad9549442ab73552a527639d23ca13
AAbdelhady <a.said.abdelhady@gmail.com>
2014-11-03 20:58:38 +0200

    Update Beverage.java

--- RestaurantGameCodeGen/src/Beverage.java
+++ RestaurantGameCodeGen/src/Beverage.java
@@ -1,17 +1,5 @@
 public class Beverage extends Menu_Item
 {
 	public int volume;
-	
-	public Beverage randomlyChooseBev( )
-	{
-		return null;
-	}
-	
-	public Beverage addNewBeverage( String bevName, Quality bevQuality, int bevPrice, int bevVol )
-	{
-		return null;
-	}
-	
-	
-	
 }
+
