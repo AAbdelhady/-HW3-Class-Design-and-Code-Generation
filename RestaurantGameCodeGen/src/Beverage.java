b164564a1616450aecd1089247a1747286dc2dc9
AAbdelhady <a.said.abdelhady@gmail.com>
2014-11-03 10:34:47 +0200

    Create Beverage.java

---
+++ RestaurantGameCodeGen/src/Beverage.java
@@ -0,0 +1,17 @@
+public class Beverage extends Menu_Item
+{
+	public int volume;
+	
+	public Beverage randomlyChooseBev( )
+	{
+		return null;
+	}
+	
+	public Beverage addNewBeverage( String bevName, Quality bevQuality, int bevPrice, int bevVol )
+	{
+		return null;
+	}
+	
+	
+	
+}
