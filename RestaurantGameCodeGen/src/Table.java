a3106a7148c6ea5252294867e6eab5df548f9da2
AAbdelhady <a.said.abdelhady@gmail.com>
2014-11-03 10:43:24 +0200

    Create Table.java

---
+++ RestaurantGameCodeGen/src/Table.java
@@ -0,0 +1,57 @@
+public class Table
+{
+	public int number;
+	
+	public Client clientsAtTable[];
+	
+	public Waiter assignedWaiter;
+	
+	
+	public int receivePaymentForTable( )
+	{
+		int totalPayment=0;
+		totalPayment+=clientsAtTable[0].payForOrder();
+		totalPayment+=clientsAtTable[1].payForOrder();
+		return totalPayment;
+	}
+	
+	public int calcRepForTable(Experience chefExp, Experience barmanExp)
+	{
+		int repFromTable = 0;
+		
+		for(int i=0;i<2;i++)
+		{
+			if(clientsAtTable[i].isSatisfiedWithService(assignedWaiter.levelOfExperience))
+			{
+				repFromTable++;
+			}
+			else
+			{
+				repFromTable--;
+			}
+			
+			if(clientsAtTable[i].isSatisfiedWithDish(chefExp))
+			{
+				repFromTable++;
+			}
+			else
+			{
+				repFromTable--;
+			}
+			
+			if(clientsAtTable[i].isSatisfiedWithBeverage(barmanExp))
+			{
+				repFromTable++;
+			}
+			else
+			{
+				repFromTable--;
+			}
+		}
+		
+		return 0;
+	}
+	
+	
+	
+}
