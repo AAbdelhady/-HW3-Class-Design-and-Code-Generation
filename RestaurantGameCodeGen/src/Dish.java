3b826fdbe2d6214df20b51b7c6abe60ead46a5d6
AAbdelhady <a.said.abdelhady@gmail.com>
2014-11-03 10:36:49 +0200

    Create Dish.java

---
+++ RestaurantGameCodeGen/src/Dish.java
@@ -0,0 +1,15 @@
+public class Dish extends Menu_Item
+{
+	public int calories;
+	
+	public Dish randomlyChooseDish( )
+	{
+		return null;
+	}
+	
+	public Dish addNewDish( String dishName, Quality dishQuality, int dishPrice, int dishCalories )
+	{
+		return null;
+	}
+	
+}
