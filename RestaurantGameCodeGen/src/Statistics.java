d30e7433cfe5e9fae7d9bc67c7337ee73b214522
AAbdelhady <a.said.abdelhady@gmail.com>
2014-11-03 10:42:59 +0200

    Create Statistics.java

---
+++ RestaurantGameCodeGen/src/Statistics.java
@@ -0,0 +1,19 @@
+public class Statistics
+{
+	private int dishAvgCalorieCount;
+	
+	private int beverageAvgVolume;
+	
+	private int moneySpent;
+	
+	private int dishCount;
+	
+	private int beverageCount;
+	
+	public void showStatistics( )
+	{
+		
+	}
+	
+	
+}
